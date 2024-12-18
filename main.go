5594da04f1968c9d313ee173c3b3d1b58b78d400
Krishkbc <210120107075@git.org.in>
2024-12-18 18:20:24 +0530

    updates of day 2

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/thedevsaddam/renderer"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -26,10 +30,10 @@ const (
 
 type (
 	TodoModel struct {
-		ID        string    `json:"id"`
-		Title     string    `json:"title"`
-		Completed bool      `json:"completed"`
-		CreatedAt time.Time `json:"created_at"`
+		ID        primitive.ObjectID `json:"id"`
+		Title     string             `json:"title"`
+		Completed bool               `json:"completed"`
+		CreatedAt time.Time          `json:"created_at"`
 	}
 
 	Todo struct {
@@ -37,6 +41,11 @@ type (
 		Title     string             `bson:"title"`
 		Completed bool               `bson:"completed"`
 		CreatedAt time.Time          `bson:"creation_at"`
+	}
+
+	GetTodoResponse struct {
+		Message string `json:"message"`
+		Data    []Todo `json:"data"`
 	}
 )
 
@@ -66,8 +75,20 @@ func init() {
 
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	filepath := "./README.md"
+	err := rnd.FileView(w, http.StatusOK, filepath, "readme.md")
+	checkerr(err)
+}
+
 func main() {
 	fmt.Println("helloo")
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", homeHandler)
+	router.Mount("/todo", todoHandler())
+
 	server := &http.Server{
 		Addr:         ":9000",
 		Handler:      chi.NewRouter(),
@@ -75,8 +96,74 @@ func main() {
 		WriteTimeout: 60 * time.Second,
 	}
 
-	fmt.Println("server starting at port 9000")
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	stopchan := make(chan os.Signal, 1)
+	signal.Notify(stopchan, os.Interrupt)
+
+	go func() {
+		fmt.Println("server starting at port 9000")
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sig := <-stopchan
+	log.Printf("shutting down server with signal: %v", sig)
+
+	if err := client.Disconnect(context.Background()); err != nil {
+		panic(err)
 	}
+	// create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// shutdown the server gracefully
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %v\n", err)
+	}
+	log.Println("Server shutdown gracefully")
+
+}
+
+func todoHandler() http.Handler {
+	router := chi.NewRouter()
+	router.Group(func(r chi.Router) {
+		r.Get("/", getTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+	})
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	var todoListFromDb = []TodoModel{}
+	filter := bson.D{}
+
+	cursor, err := db.Collection(collectionName).Find(context.Background(), filter)
+	if err != nil {
+		log.Printf("failed to fetch todo records from the db: %v\n", err.Error())
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message": "Could not fetch the todo collection",
+			"error":   err.Error(),
+		})
+	}
+
+	todoList := []Todo{}
+	if err := cursor.All(context.Background(), &todoListFromDb); err != nil {
+		checkerr(err)
+	}
+
+	for _, td := range todoListFromDb {
+		todoList = append(todoList, Todo{
+			ID:        td.ID,
+			Title:     td.Title,
+			Completed: td.Completed,
+			CreatedAt: td.CreatedAt,
+		})
+	}
+
+	rnd.JSON(w, http.StatusOK, GetTodoResponse{
+		Message: "Todo list fetched successfully",
+		Data:    todoList,
+	})
+
 }
